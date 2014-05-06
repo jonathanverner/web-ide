bb6c4541cb534de9c9c3de8501477df1746af9a6
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2014-05-06 11:00:23 +0200

    Add local server.
      -- a grunt-task to run a local server.
      -- a script to run a simple python server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,17 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
+    grunt.initConfig(
+    {
+        'http-server': {
+            'dev': {
+                root:'.',
+                port:8000,
+                host:"127.0.0.1",
+                showDir:true,
+                autoIndex:true,
+                defaultExt:"html",
+                runInBackground:false
+            }
+        },
         intern: {
             someReleaseTarget: {
                 options: {
@@ -12,11 +24,15 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-http-server');
+    grunt.registerTask('serve',['http-server:dev']);
+
     // Load the Intern task
     grunt.loadNpmTasks('intern');
 
     // Register a test task that uses Intern
     grunt.registerTask('test', [ 'intern' ]);
+
 
     // By default we just test
     grunt.registerTask('default', [ 'test' ]);
