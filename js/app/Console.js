4f1bcfecdc06b21ad5e12d2abc7eda85104035e3
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2014-05-09 17:06:57 +0200

    Wrap the jq-console (from repl.it) into a suitable interface.

--- js/app/Console.js
+++ js/app/Console.js
@@ -1,3 +1,53 @@
-function Console() {
+define(["jq-console/jqconsole", "app/Signals"], function( jqc, s ) {
+    return {
+        Console:function (element) {
+            var cel = document.createElement("div");
+            cel.classList.add("jqconsole-elem");
+            element.appendChild(cel);
+            var c = new jqc.console(cel,"",">>> ","... ");
+            var self = this;
 
-}
+            this.modes = {
+                INPUT:0,
+                OUTPUT:1,
+                PROMPT:2
+            };
+            var mode = this.modes.OUTPUT;
+
+            this.write = function (string, cls) {
+                if ( mode == this.modes.OUTPUT ) c.Write(string, cls);
+                else throw "Cannot write in mode "+mode;
+            };
+
+            this.readline = function (input_handler) {
+                if ( mode != this.modes.OUTPUT ) throw "Cannot input in mode "+mode;
+                mode = this.modes.INPUT;
+                self = this
+                c.Input(function (input) {
+                    mode = self.modes.OUTPUT;
+                    if (input_handler === undefined ) self.readline_finished.emit(input);
+                    else input_handler(input);
+                });
+            };
+
+            this.prompt = function() {
+                if ( mode == this.modes.PROMPT ) return;
+                if ( mode == this.modes.INPUT ) throw "Cannot prompt in input mode";
+                mode = this.modes.PROMPT;
+                c.Prompt(true, function(cmd) {
+                    mode = self.modes.OUTPUT;
+                    self.command_input.emit(cmd);
+                }, this.test_multiline);
+            };
+
+            this.test_multiline = function (input) {
+                return false; /* or Number @N --- adjust indent by @N */
+            };
+
+            this.command_input = new s.Signal("command");
+            this.readline_finished = new s.Signal("input");
+
+        }
+    }
+});
+
--- js/app/main.js
+++ js/app/main.js
@@ -5,8 +5,15 @@ requirejs.config({
     }
 });
 
-requirejs(["app/FileSystem", "app/Store", "app/MemStore"], function (FS, Store, MS) {
+requirejs(["app/FileSystem", "app/Store", "app/MemStore","jquery","jq-console/jqconsole","app/Console","app/Worker","app/utils"],
+          function (FS, Store, MS, $, jqc, con, worker,u) {
     window.FS = FS;
     window.Store = Store;
     window.s = new MS.MemStore();
+    window.jqc = jqc;
+    window.con = con;
+    var ce = document.getElementsByClassName("output")[0]
+    window.cons = new window.con.Console(ce);
+    window.cons.write("Hello & welcome\n");
+    window.cons.prompt();
 });
