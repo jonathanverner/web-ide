3f89f3721233fb517a1e776bc1e5b7931bb643d8
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2014-05-09 17:11:52 +0200

    Remove unused files; augment to .gitignor.

--- js/app/Stream.js
+++
@@ -1,9 +0,0 @@
-function Stream(name) {
-    this.name = name;
-    this.write = function (data) {
-        $.publish(this.name, data);
-    }
-    this.flush = function () {};
-    this.close = function () {};
-    this.readline = function () {};
-}
