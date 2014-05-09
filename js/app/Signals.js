205d034d86cc6ea3046fb8a7f077a2a2e473d9e4
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2014-05-09 17:43:40 +0200

    BugFix: Make the Singal class definition available in Signal.js.

--- js/app/Signals.js
+++ js/app/Signals.js
@@ -1,4 +1,38 @@
 define(function() {
+    var Signal = function() {
+            var slots = [];
+            var arg_len = arguments.length;
+            var recursion_check = 0;
+
+
+            // Connects signal handler @slot to this signal
+            this.connect = function(slot, object) {
+                if (slot.length > arg_len) throw new SignalException("Wrong slot signature ("+slot.length+" instead of "+arg_len+").");
+                slots.push({s:slot,o:object});
+                if (slot.__signals__ === undefined) slot.__signals__ = [];
+                slot.__signals__.push(this);
+            }
+            this.emit = function () {
+                var i;
+                if (arguments.length != arg_len) throw new SignalException("Wrong signal signature (expecting "+arg_len+" got "+ arguments.length+" arguments");
+                for(i = 0; i < slots.length; i++) {
+                        if ( recursion_check > 3 ) throw new SignalException("Recursion depth exceeded limit "+recursion_check);
+                        recursion_check++;
+                        slots[i].s.apply(slots[i].o,arguments);
+                        recursion_check--;
+                }
+            }
+            this.disconnect = function(slot) {
+                var i,nslots = [];
+                for(i = 0; i < slots.length; i++) {
+                    if (! slots[i].s === slot) nslots.push(slots[i]);
+                }
+                slots = nslots;
+            }
+            this.disconnect_all = function() {
+                slots = [];
+            }
+    };
     return {
         SignalException: function(message) {
             this.message = message;
@@ -20,40 +54,6 @@ define(function() {
                 if (obj[attr] instanceof Signal) obj[attr].disconnect_all();
             }
         },
-        Signal: function() {
-            var slots = [];
-            var arg_len = arguments.length;
-            var recursion_check = 0;
-
-
-            // Connects signal handler @slot to this signal
-            this.connect = function(slot, object) {
-                if (slot.length > arg_len) throw new SignalException("Wrong slot signature ("+slot.length+" instead of "+arg_len+").");
-                slots.push({s:slot,o:object});
-                if (slot.__signals__ === undefined) slot.__signals__ = [];
-                slot.__signals__.push(this);
-            }
-            this.emit = function () {
-                var i;
-                if (arguments.length != arg_len) throw new SignalException("Wrong signal signature (expecting "+arg_len+" got "+ arguments.length+" arguments");
-                for(i = 0; i < slots.length; i++) {
-                        if ( recursion_check > 3 ) throw new SignalException("Recursion depth exceeded limit "+recursion_check);
-                        recursion_check++;
-                        slots[i].s.apply(slots[i].o,arguments);
-                        recursion_check--;
-
-                }
-            }
-            this.disconnect = function(slot) {
-                var i,nslots = [];
-                for(i = 0; i < slots.length; i++) {
-                    if (! slots[i].s === slot) nslots.push(slots[i]);
-                }
-                slots = nslots;
-            }
-            this.disconnect_all = function() {
-                slots = [];
-            }
-        }
+        Signal: Signal
     }
 });
